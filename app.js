1ca7d8da403756eba0d17b9960d9445c0b9b6efd
Ankur Raj <ankur.jar123@gmail.com>
2021-07-20 21:40:27 +0530

    Initial Project setup

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const flash = require('connect-flash');
+const session = require('express-session');
+const dotenv = require('dotenv')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
+
